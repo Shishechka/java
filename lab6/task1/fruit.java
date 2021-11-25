5d5c6d9773ceeea1824a8a7dd2512bd416e9c4f3
Shishechka <47379049+Shishechka@users.noreply.github.com>
2021-11-25 14:48:09 +0300

    Create fruit.java

---
+++ lab6/task1/fruit.java
@@ -0,0 +1,14 @@
+package lab6.task1;
+
+public abstract class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
