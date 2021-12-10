2aa3a1289514e86810a886e09e962a1b5b61e1a1
Shishechka <47379049+Shishechka@users.noreply.github.com>
2021-12-10 13:05:42 +0300

    Create Category.java

---
+++ lab7/Category.java
@@ -0,0 +1,11 @@
+package lab7;
+
+public class Category {
+    String nameCategory;
+    Product[] products;
+
+    Category(String nameCategory, Product[] products) {
+        this.nameCategory = nameCategory;
+        this.products = products;
+    }
+}
