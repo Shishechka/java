799cdfc37cf8a6ec11cfd981a4544e945cbdc4ba
Shishechka <47379049+Shishechka@users.noreply.github.com>
2021-12-17 12:37:08 +0300

    Delete main.java

--- lab9/main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i < 4; i++) {
-            new Thread(new MyThread(i), String.valueOf(i)).start();
-        }
-    }
-}
