e1aa2243ffdfc2b017561898b81915ba181eae71
Shishechka <47379049+Shishechka@users.noreply.github.com>
2021-12-17 12:38:42 +0300

    Create MyThread.java

---
+++ lab9/task1/MyThread.java
@@ -0,0 +1,24 @@
+public class MyThread implements Runnable {
+
+    int j;
+
+    MyThread(int j){
+        this.j = j;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 10 == 0) {
+                System.out.println(Thread.currentThread().getName() + " thread:\t" + i + " делится на 10 без остатка");
+                try {
+                    Thread.sleep(500*j);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Мой поток " + this.j + " завершился.");
+        
+    }
+}
